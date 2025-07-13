4af309009e25a59c80b5793809544b1d74334bc5
shivam pandey <you@example.com>
2025-07-13 16:49:58 +0530

    add simple calculator

---
+++ calculator.py
@@ -0,0 +1,6 @@
+#ask the user for two numbers
+num1= float(input ("enter first number :"))
+num2=float(input ("enter seconf number :"))
+# add the numbers 
+result = num1+num2 
+print ("the sum is :",result)
